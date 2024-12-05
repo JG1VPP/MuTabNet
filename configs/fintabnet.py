dffbffdcd17714399fcd22b79a3814c15214f729
JG1VPP <jg1vpp@gmail.com>
2024-12-05 17:51:47 +0900

    pipeline

--- configs/fintabnet.py
+++ configs/fintabnet.py
@@ -8,14 +8,47 @@ model = dict(
     )
 )
 
+train_pipeline = [
+    dict(type="LoadImageFromFile"),
+    dict(type="TableResize", size=520),
+    dict(
+        type="TablePad",
+        size=(520, 520),
+    ),
+    dict(type="TableBboxFlip"),
+    dict(type="TableBboxEncode"),
+    dict(type="ToTensorOCR"),
+    dict(
+        type="NormalizeOCR",
+        mean=[0.5, 0.5, 0.5],
+        std=[0.5, 0.5, 0.5],
+    ),
+    dict(
+        type="Collect",
+        keys=["img"],
+        meta_keys=[
+            "filename",
+            "ori_shape",
+            "img_shape",
+            "pad_shape",
+            "img_scale",
+            "html",
+            "cell",
+            "bbox",
+        ],
+    ),
+]
+
 data = dict(
     train=dict(
         img_prefix="../data/fintabnet/img_tables/train/",
         ann_file="../data/mmocr_fintabnet/train/",
+        pipeline=train_pipeline,
     ),
     val=dict(
         img_prefix="../data/fintabnet/img_tables/test/",
         ann_file="../data/mmocr_fintabsub/test/",
+        pipeline=train_pipeline,
     ),
     test=dict(
         img_prefix="../data/fintabnet/img_tables/val/",
